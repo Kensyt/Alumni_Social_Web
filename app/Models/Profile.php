f19445ec363c3cc52f2d9ee0ec88928f4b1edecc
Kensyt <khidalgo19.kh@gmail.com>
2020-09-17 15:20:43 +0200

    new model Profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Rol extends Model
+class Profile extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'username', 'avatar', 'git', 'linkend','link'
-    ];
 }
